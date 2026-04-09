17f9b6baf76ab63babb9500bac7d8faeec5fbd70
butlers1 <butlers1@byu.edu>
2026-04-09 13:15:11 -0600

    Remove tracked build artifacts from cookies merge

--- backend/obj/Debug/net10.0/OpenArms.Api.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/OpenArms.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenArms.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b6c6106de6c863a70756b8d47d2fadd458a6fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+592ea2a470ab988dc946cd3b3088c21b6e45fd02")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenArms.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenArms.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
